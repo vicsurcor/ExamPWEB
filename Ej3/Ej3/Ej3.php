e6e0a0704d92e00b83770d047d4f3aaed4f01f8e
vicsurcor <vdsurcor@gmail.com>
2024-06-07 18:48:10 +0200

    Fin

--- Ej3/Ej3/Ej3.php
+++ Ej3/Ej3/Ej3.php
@@ -1,6 +1,7 @@
 <?php
+$usuario = trim($_POST["usuario"]);
 if (!isset($_COOKIE['saludo'])) {
-    $saludo = "Buenos dias + Usuario(Azul)";
+    $saludo = "Buenos dias + " .  $usuario;
     setcookie('saludo', $saludo, time() + 3600); // Cookie lasts for 1 hour
     header('Location: Ej3.html');
     exit();
@@ -9,10 +10,10 @@ if (!isset($_COOKIE['saludo'])) {
     $change = isset($_POST['change']) ? $_POST['change'] : 'No';
 
     if ($change == 'Si') {
-        if ($saludo == "Buenos dias + Usuario(Azul)") {
-            $saludo = "Hola + Usuario(Verde)";
-        } elseif ($saludo == "Hola + Usuario(Verde)") {
-            $saludo = "Buenos dias + Usuario(Azul)";
+        if (strpos ($saludo , "Buenos dias") !== false) {
+            $saludo = "Hola + " . $usuario;
+        } elseif (strpos ($saludo , "Hola") !== false) {
+            $saludo = "Buenos dias + " . $usuario;
         }
         setcookie('saludo', $saludo, time() + 3600); // Cookie lasts for 1 hour
     }
